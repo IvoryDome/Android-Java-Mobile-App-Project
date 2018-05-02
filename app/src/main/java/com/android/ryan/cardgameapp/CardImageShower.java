bc12da5b6b18546b9f4a27f166faf956b771bb45
Ryan Mackay <ryanmackay@protonmail.com>
2018-05-02 10:11:15 +0100

    refactored image showing code, added new class

--- app/src/main/java/com/android/ryan/cardgameapp/CardImageShower.java
+++ app/src/main/java/com/android/ryan/cardgameapp/CardImageShower.java
@@ -1,4 +1,177 @@
 package com.android.ryan.cardgameapp;
 
+import android.widget.ImageView;
+
 public class CardImageShower {
+
+
+    public static void CardImageShower() {
+    }
+
+    public void assignCardImage(String card, ImageView image) {
+        switch (card) {
+            case "ACE of SPADES":
+                image.setImageResource(R.drawable.sa);
+                break;
+            case "TWO of SPADES":
+                image.setImageResource(R.drawable.s2);
+                break;
+            case "THREE of SPADES":
+                image.setImageResource(R.drawable.s3);
+                break;
+            case "FOUR of SPADES":
+                image.setImageResource(R.drawable.s4);
+                break;
+            case "FIVE of SPADES":
+                image.setImageResource(R.drawable.s5);
+                break;
+            case "SIX of SPADES":
+                image.setImageResource(R.drawable.s6);
+                break;
+            case "SEVEN of SPADES":
+                image.setImageResource(R.drawable.s7);
+                break;
+            case "EIGHT of SPADES":
+                image.setImageResource(R.drawable.s8);
+                break;
+            case "NINE of SPADES":
+                image.setImageResource(R.drawable.s9);
+                break;
+            case "TEN of SPADES":
+                image.setImageResource(R.drawable.s10);
+                break;
+            case "JACK of SPADES":
+                image.setImageResource(R.drawable.sj);
+                break;
+            case "QUEEN of SPADES":
+                image.setImageResource(R.drawable.sq);
+                break;
+            case "KING of SPADES":
+                image.setImageResource(R.drawable.sk);
+                break;
+            case "ACE of DIAMONDS":
+                image.setImageResource(R.drawable.da);
+                break;
+            case "TWO of DIAMONDS":
+                image.setImageResource(R.drawable.d2);
+                break;
+            case "THREE of DIAMONDS":
+                image.setImageResource(R.drawable.d3);
+                break;
+            case "FOUR of DIAMONDS":
+                image.setImageResource(R.drawable.d4);
+                break;
+            case "FIVE of DIAMONDS":
+                image.setImageResource(R.drawable.d5);
+                break;
+            case "SIX of DIAMONDS":
+                image.setImageResource(R.drawable.d6);
+                break;
+            case "SEVEN of DIAMONDS":
+                image.setImageResource(R.drawable.d7);
+                break;
+            case "EIGHT of DIAMONDS":
+                image.setImageResource(R.drawable.d8);
+                break;
+            case "NINE of DIAMONDS":
+                image.setImageResource(R.drawable.d9);
+                break;
+            case "TEN of DIAMONDS":
+                image.setImageResource(R.drawable.d10);
+                break;
+            case "JACK of DIAMONDS":
+                image.setImageResource(R.drawable.dj);
+                break;
+            case "QUEEN of DIAMONDS":
+                image.setImageResource(R.drawable.dq);
+                break;
+            case "KING of DIAMONDS":
+                image.setImageResource(R.drawable.dk);
+                break;
+            case "ACE of CLUBS":
+                image.setImageResource(R.drawable.ca);
+                break;
+            case "TWO of CLUBS":
+                image.setImageResource(R.drawable.c2);
+                break;
+            case "THREE of CLUBS":
+                image.setImageResource(R.drawable.c3);
+                break;
+            case "FOUR of CLUBS":
+                image.setImageResource(R.drawable.c4);
+                break;
+            case "FIVE of CLUBS":
+                image.setImageResource(R.drawable.c5);
+                break;
+            case "SIX of CLUBS":
+                image.setImageResource(R.drawable.c6);
+                break;
+            case "SEVEN of CLUBS":
+                image.setImageResource(R.drawable.c7);
+                break;
+            case "EIGHT of CLUBS":
+                image.setImageResource(R.drawable.c8);
+                break;
+            case "NINE of CLUBS":
+                image.setImageResource(R.drawable.c9);
+                break;
+            case "TEN of CLUBS":
+                image.setImageResource(R.drawable.c10);
+                break;
+            case "JACK of CLUBS":
+                image.setImageResource(R.drawable.cj);
+                break;
+            case "QUEEN of CLUBS":
+                image.setImageResource(R.drawable.cq);
+                break;
+            case "KING of CLUBS":
+                image.setImageResource(R.drawable.ck);
+                break;
+            case "ACE of HEARTS":
+                image.setImageResource(R.drawable.ha);
+                break;
+            case "TWO of HEARTS":
+                image.setImageResource(R.drawable.h2);
+                break;
+            case "THREE of HEARTS":
+                image.setImageResource(R.drawable.h3);
+                break;
+            case "FOUR of HEARTS":
+                image.setImageResource(R.drawable.h4);
+                break;
+            case "FIVE of HEARTS":
+                image.setImageResource(R.drawable.h5);
+                break;
+            case "SIX of HEARTS":
+                image.setImageResource(R.drawable.h6);
+                break;
+            case "SEVEN of HEARTS":
+                image.setImageResource(R.drawable.h7);
+                break;
+            case "EIGHT of HEARTS":
+                image.setImageResource(R.drawable.h8);
+                break;
+            case "NINE of HEARTS":
+                image.setImageResource(R.drawable.h9);
+                break;
+            case "TEN of HEARTS":
+                image.setImageResource(R.drawable.h10);
+                break;
+            case "JACK of HEARTS":
+                image.setImageResource(R.drawable.hj);
+                break;
+            case "QUEEN of HEARTS":
+                image.setImageResource(R.drawable.hq);
+                break;
+            case "KING of HEARTS":
+                image.setImageResource(R.drawable.hk);
+                break;
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
